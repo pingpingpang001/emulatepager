638d2160f97f5d1f3bedae02fae5e05425f7698e
marvin--liu <529519366@qq.com>
2016-08-17 11:07:52 +0800

    init

---
+++ app/src/main/java/sf/hmg/turntest/turntest.java
@@ -0,0 +1,114 @@
+package sf.hmg.turntest;
+
+import java.io.File;
+import java.io.IOException;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class turntest extends Activity {
+	/** Called when the activity is first created. */
+	private PageWidget mPageWidget;
+	Bitmap mCurPageBitmap, mNextPageBitmap;
+	Canvas mCurPageCanvas, mNextPageCanvas;
+	BookPageFactory pagefactory;
+
+	WindowManager wm;
+	int width = 0;
+	int height = 0;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		mPageWidget = new PageWidget(this);
+		setContentView(mPageWidget);
+
+		wm = this.getWindowManager();
+		width = wm.getDefaultDisplay().getWidth();
+		height = wm.getDefaultDisplay().getHeight();
+
+		mCurPageBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+		mNextPageBitmap = Bitmap
+				.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+
+		mCurPageCanvas = new Canvas(mCurPageBitmap);
+		mNextPageCanvas = new Canvas(mNextPageBitmap);
+		pagefactory = new BookPageFactory(width, height);
+
+		pagefactory.setBgBitmap(BitmapFactory.decodeResource(
+				this.getResources(), R.drawable.bg));
+
+		try {
+			File file = new File(Environment.getExternalStorageDirectory(),"test.txt");
+			Log.e("file path", file.getAbsolutePath());
+			pagefactory.openbook(file.getPath());
+			pagefactory.onDraw(mCurPageCanvas);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			Toast.makeText(this, "电子书不存在,请将《test.txt》放在SD卡根目录下",
+					Toast.LENGTH_SHORT).show();
+		}
+
+		mPageWidget.setBitmaps(mCurPageBitmap, mCurPageBitmap);
+
+		mPageWidget.setOnTouchListener(new OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent e) {
+				// TODO Auto-generated method stub
+
+				boolean ret=false;
+				if (v == mPageWidget) {
+					if (e.getAction() == MotionEvent.ACTION_DOWN) {
+						mPageWidget.abortAnimation();
+						mPageWidget.calcCornerXY(e.getX(), e.getY());
+
+						pagefactory.onDraw(mCurPageCanvas);
+						if (mPageWidget.DragToRight()) {
+							try {
+								pagefactory.prePage();
+							} catch (IOException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+							if(pagefactory.isfirstPage())return false;
+							pagefactory.onDraw(mNextPageCanvas);
+//							mPageWidget.calcCornerXY(e.getX(), e.getY(), false);
+						} else {
+							try {
+								pagefactory.nextPage();
+							} catch (IOException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+							if(pagefactory.islastPage())return false;
+							pagefactory.onDraw(mNextPageCanvas);
+//							mPageWidget.calcCornerXY(e.getX(), e.getY(), true);
+						}
+						mPageWidget.setBitmaps(mCurPageBitmap, mNextPageBitmap);
+					}
+
+					ret = mPageWidget.doTouchEvent(e);
+					return ret;
+				}
+				return false;
+			}
+
+		});
+	}
+}
